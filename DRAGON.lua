453b7fa736e5a86a34d11165196190bca774b7b9
moohamed10 <87588551+moohamed10@users.noreply.github.com>
2021-07-18 03:59:20 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -163,7 +163,7 @@ io.popen("mkdir File_Bot")
 io.popen("cd File_Bot && rm -rf commands.lua.1") 
 io.popen("cd File_Bot && rm -rf commands.lua.2") 
 io.popen("cd File_Bot && rm -rf commands.lua.3") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/moohamed10/rogen/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/moohamed10/roogen/main/File_Bot/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
@@ -1176,7 +1176,7 @@ echo '•┄─━━━━•𝐑𝐎𝐎𝐆𝐄𝐍•━━━━─┄•\n
 end
 if text == 'تحديث السورس ' and DevSoFi(msg) then 
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/moohamed10/rogen/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/moohamed10/roogen/main/DRAGON.lua')
 send(msg.chat_id_, msg.id_,' ◉ تم تحديث السورس')
 dofile('DRAGON.lua')  
 end
@@ -2814,7 +2814,7 @@ end
 return false
 end
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/moohamed10/rogen/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/moohamed10/roogen/main/DRAGON.lua')
 send(msg.chat_id_, msg.id_,' ◉ تم تحديث السورس')
 dofile('DRAGON.lua')  
 end
@@ -5741,7 +5741,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if DevSoFi(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/moohamed10/rogen/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/moohamed10/roogen/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -5779,7 +5779,7 @@ t = " ◉ الملف  ⇇"..file.."\n ◉ تم تعطيل ملف \n"
 else
 t = " ◉ بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/moohamed10/rogen/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/moohamed10/roogen/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -5799,7 +5799,7 @@ t = " ◉ بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " ◉ الملف  ⇇"..file.."\n ◉ تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/moohamed10/rogen/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/moohamed10/roogen/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
